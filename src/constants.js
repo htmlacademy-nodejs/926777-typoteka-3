612ef3f7cb21cc420e7b076722ffd58e2ef8e925
AlexyShu <Za9i@bk.ru>
2021-03-10 10:46:33 +0300

    fixed module2-task3

--- src/constants.js
+++ src/constants.js
@@ -9,3 +9,11 @@ module.exports.ExitCode = {
   ERROR: 1,
   SUCCESS: 0,
 };
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -13,6 +13,7 @@ module.exports = {
     --version:            выводит номер версии
     --help:               печатает этот текст
     --generate <count>    формирует файл mocks.json
+    --server 3000         запускает сервер
     `;
     console.log(chalk.gray(text));
   }
