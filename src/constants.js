d021564971eccfe27c1d0ccff14932bd3d01d87a
AlexyShu <Za9i@bk.ru>
2021-05-10 21:37:40 +0300

    modele4-task1 1

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,8 @@ module.exports.DEFAULT_COMMAND = `--help`;
 // 3ий аргумент process.argv[2]
 module.exports.USER_ARGV_INDEX = 2;
 
+module.exports.MAX_ID_LENGTH = 6;
+
 module.exports.ExitCode = {
   ERROR: 1,
   SUCCESS: 0,
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,6 +2,7 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
+const {nanoid} = require(`nanoid`);
 
 const {
   getRandomInt,
@@ -9,19 +10,34 @@ const {
   randomDate
 } = require(`../../utils`);
 
+const {MAX_ID_LENGTH} = require(`../../constants`);
+
 const DEFAULT_COUNT = 1;
+const MAX_COMMENTS = 4;
 const FILE_NAME = `mocks.json`;
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+const FILE_COMMENTS_PATH = `./data/comments.txt`;
 
-const generatePublication = (count, titles, categories, sentences) => (
+const generateComments = (count, comments) => (
   Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `),
+  }))
+);
+
+const generatePublication = (count, titles, categories, sentences, comments) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
     title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: randomDate(`01/02/2021`, `01/05/2021`),
     announce: shuffle(sentences).slice(1, 5).join(` `),
     fullText: shuffle(sentences).slice(1, 5).join(` `),
     сategory: [categories[getRandomInt(0, categories.length - 1)]],
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
   }))
 );
 
@@ -41,9 +57,10 @@ module.exports = {
     const sentences = await readContent(FILE_SENTENCES_PATH);
     const titles = await readContent(FILE_TITLES_PATH);
     const categories = await readContent(FILE_CATEGORIES_PATH);
+    const comments = await readContent(FILE_COMMENTS_PATH);
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePublication(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(generatePublication(countOffer, titles, categories, sentences, comments));
     if (args > 1000) {
       console.info(chalk.red(`Limit exceeded. Maximum 1000`));
     } else {
