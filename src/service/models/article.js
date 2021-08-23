2029cb6e3570d3f9db640a19b8e2de4cd6a4a375
AlexyShu <Za9i@bk.ru>
2021-08-23 09:56:23 +0300

    создаёт Модели Sequelize

---
+++ src/service/models/article.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const {DataTypes, Model} = require(`sequelize`);
+
+class Article extends Model {}
+
+const define = (sequelize) => Article.init({
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  fullText: {
+    type: DataTypes.STRING,
+    field: `full_text`,
+    allowNull: false
+  },
+  anounce: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  picture: DataTypes.STRING,
+  createdDate: {
+    type: DataTypes.DATE,
+    field: `create_date`,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: `Article`,
+  tableName: `articles`
+});
+
+module.exports = define;
